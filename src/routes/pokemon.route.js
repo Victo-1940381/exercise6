46aa6ec550c3b0399196eea593f1cc52298f525e
ludo <1940381@carrefour.cegepvicto.ca>
2025-02-18 09:52:36 -0500

    route id marche a nouveau

--- src/routes/pokemon.route.js
+++ src/routes/pokemon.route.js
@@ -2,8 +2,8 @@ import express from 'express';
 import db from '../config/db.js';
 import { trouverpokemon,trouverpokemonlist } from '../controller/pokemon.controller.js';
 const router = express.Router();
-
-//router.get('/:id', trouverpokemon);
 router.get('/liste', trouverpokemonlist);
+router.get('/:id', trouverpokemon);
+
 
 export default router;
