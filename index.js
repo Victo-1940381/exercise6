84a18089d1f039b5aee337cdf43441b320081550
ludo <1940381@carrefour.cegepvicto.ca>
2025-02-21 10:15:13 -0500

    debut docs

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import router from './src/routes/pokemon.route.js';
-
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+const swaggerDocument = JSON.parse(fs.readFileSync('./src/config/documentation.json', 'utf8'));
+const swaggerOptions = {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "pokemon api"
+};
 // Créer une application express
 const app = express();
 
 // Importer les middlewares
 app.use(express.json());
 app.use('/api/pokemon', router);
-
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
 
 
 
